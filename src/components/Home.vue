d2dcf1c5b81ecb16b73c9710ef2bd5810c01f097
Quentin Bernet <quentinbernet@MacBook-Pro-de-Quentin.local>
2019-01-02 13:53:39 +0100

    :heavy_plus_sign: Ajout d'un composant pour la page d'accueil

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="accueil">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      msg: 'Accueil',
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
   routes: [
@@ -11,5 +12,10 @@ export default new Router({
       name: 'HelloWorld',
       component: HelloWorld,
     },
+    {
+      path: '/accueil',
+      name: 'Home',
+      component: Home,
+    },
   ],
-});
+})
