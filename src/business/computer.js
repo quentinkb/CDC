7da64319871dc138cf38e82d537ba1232cc2505a
Quentin Bernet <quentinbernet@macbook-pro-de-quentin.home>
2019-01-06 00:02:48 +0100

    recup valeur dés et calculs

--- src/business/computer.js
+++ src/business/computer.js
@@ -1,1 +1,27 @@
-export default class Computer {}
+export default class Computer {
+  constructor(val1, val2, val3) {
+    this.val1 = parseInt(val1, 10)
+    this.val2 = parseInt(val2, 10)
+    this.val3 = parseInt(val3, 10)
+  }
+  compute() {
+    this.checkChouette()
+  }
+  checkChouette() {
+    return (
+      this.val1 === this.val2 ||
+      this.val2 === this.val3 ||
+      this.val1 === this.val3
+    )
+  }
+  checkVelutte() {
+    return (
+      this.val2 + this.val3 === this.val1 ||
+      this.val1 + this.val3 === this.val2 ||
+      this.val1 + this.val2 === this.val3
+    )
+  }
+  checkCDC() {
+    return this.val1 === this.val2 && this.val2 === this.val3
+  }
+}
--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <h1>{{ getCurrentPlayerIndex }}</h1>Dé 1
-    <input type="number">
+    <input type="number" v-on:change="diceOne=$event.target.value">
     Dé 2
-    <input type="number">
+    <input type="number" v-on:change="diceTwo=$event.target.value">
     Dé 3
-    <input type="number">
+    <input type="number" v-on:change="diceThree=$event.target.value">
     <button @click="compute()">Prochain joueur</button>
   </div>
 </template>
@@ -15,6 +15,13 @@ import Computer from './../business/computer'
 
 export default {
   name: 'Game',
+  data() {
+    return {
+      diceOne: 0,
+      diceTwo: 0,
+      diceThree: 0,
+    }
+  },
   computed: {
     getCurrentPlayerIndex() {
       return this.$store.state.currentPlayerIndex
@@ -22,7 +29,8 @@ export default {
   },
   methods: {
     compute() {
-      const computer = new Computer()
+      const computer = new Computer(this.diceOne, this.diceTwo, this.diceThree)
+      computer.compute()
       this.incrementCurrentPlayerIndex()
     },
     incrementCurrentPlayerIndex() {
